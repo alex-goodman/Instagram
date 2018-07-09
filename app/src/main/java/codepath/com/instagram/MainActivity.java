2b9619134ffea95ada24b5949035188070d89dbf
Alex Goodman <alexgood@stanford.edu>
2018-07-09 15:32:36 -0700

    Parse client working for signup/login

--- app/src/main/java/codepath/com/instagram/MainActivity.java
+++ app/src/main/java/codepath/com/instagram/MainActivity.java
@@ -1,13 +1,102 @@
 package codepath.com.instagram;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etUsername) EditText etUsername;
+    @BindView(R.id.etPassword) EditText etPassword;
+    @BindView(R.id.etEmail) EditText etEmail;
+    @BindView(R.id.btLogin) Button btLogin;
+    @BindView(R.id.btSignUp) Button btSignUp;
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        context = getApplicationContext();
+
+        btSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // get the inputted info from the text fields
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String email = etEmail.getText().toString();
+
+                // create a new user with the above attributes
+                signUp(username, password, email);
+            }
+        });
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // get the inputted info from the text fields
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                // log in with the specified credentials
+                login(username, password);
+            }
+        });
+    }
+
+    private void signUp(String username, String password, String email) {
+        // create a new user
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+
+        // sign the user up in the background
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(getApplicationContext(), "signup successful!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context, HomeActivity.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    Toast.makeText(context, "login successful", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context, HomeActivity.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/codepath/com/instagram/ParseApplication.java
@@ -0,0 +1,33 @@
+package codepath.com.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // debug logging
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+        // Use for monitoring Parse OkHttp traffic
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // initialize Parse SDK
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.appId))
+                .clientKey(null)
+                .clientBuilder(builder)
+                .server(getString(R.string.serverURL)).build());
+    }
+}
